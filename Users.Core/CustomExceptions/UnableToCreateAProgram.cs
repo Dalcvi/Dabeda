9154ef3f679125d1001f9ff74324a43ca6d596d1
dankaz1 <dangis.kazukauskas@ktu.edu>
2021-03-12 17:58:52 +0200

    Created a folder for custom exceptions

---
+++ Users.Core/CustomExceptions/UnableToCreateAProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Users.Core.CustomExceptions
+{
+    public class UnableToCreateAProgram : Exception
+    {
+        public UnableToCreateAProgram()
+        {
+        }
+
+        public UnableToCreateAProgram(string message) : base(message)
+        {
+        }
+
+        public UnableToCreateAProgram(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnableToCreateAProgram(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
