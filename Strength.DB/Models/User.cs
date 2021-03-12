efb2810ff26ed182b5bd6f898c1a95c2ff59e95a
dankaz1 <dangis.kazukauskas@ktu.edu>
2021-03-12 17:58:26 +0200

    Tweaked models information

--- Strength.DB/Models/User.cs
+++ Strength.DB/Models/User.cs
@@ -1,5 +1,4 @@
 ﻿using Strength.DB.Models;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -8,7 +7,7 @@ namespace Strength.DB
     public class User
     {
         [Key]
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
