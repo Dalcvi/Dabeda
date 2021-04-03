b94e7a22445285f03eee15c96fa426a6f1c4bc50
dankaz1 <dangis.kazukauskas@ktu.edu>
2021-04-03 18:07:34 +0300

    Added username changing feature

---
+++ Users.Core/DTO/NicknameChangeModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Users.Core.DTO
+{
+    public class NicknameChangeModel
+    {
+        public string Username { get; set; }
+    }
+}
