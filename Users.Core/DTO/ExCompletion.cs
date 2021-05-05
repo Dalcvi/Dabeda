3b242706f46c6c0f694b2b76a50b700cbdf6d449
dankaz1 <dangis.kazukauskas@ktu.edu>
2021-05-05 23:45:36 +0300

    It does not contain property "string Name" anymore; exercises repitions are saved as strings, rather than int[]

--- Users.Core/DTO/ExCompletion.cs
+++ Users.Core/DTO/ExCompletion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace Users.Core.DTO
@@ -6,8 +7,20 @@ namespace Users.Core.DTO
     public class ExCompletion
     {
         public int Id { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime? Date { get; set; }
         public Exercise Exercise { get; set; }
-        public ICollection<Set> Sets { get; set; }
+        public int Weight { get; set; }
+        public string Reps { get; set; }
+
+
+        [JsonConstructor]
+        public ExCompletion(int id = 0, DateTime? date = null, int weight = 0, string reps = null, Exercise exercise = null)
+        {
+            Id = id;
+            Date = date;
+            Exercise = exercise;
+            Weight = weight;
+            Reps = reps;
+        }
     }
 }
